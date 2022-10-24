d02d8ca8a2e721e84c652373bd41df4f766e145c
htereski <106633455+htereski@users.noreply.github.com>
2022-10-24 20:02:42 -0300

    código refatorado

--- advinhaNumero.c
+++ advinhaNumero.c
@@ -1,58 +1,98 @@
 #include <stdio.h>
 #include <time.h>
-#include <stdlib.h>
+
+int criarNumero(){
+    
+    srand(time(NULL));
+    int numero = 1 + rand() % 98;
+    return numero;    
+}
+
+int validaEntrada(int num, int min, int max){
+
+    if(num < 1 || num > 99){
+        return 0;
+    }
+    else if(num <= min || num >= max){
+        return 0;
+    }
+    return 1;
+}
+
+int entradaJogador(int i, int min, int max){
+
+    int num;
+    int valida;
+
+    do{
+        printf("Tentativa [%d] Digite um numero entre %2d e %2d: ", i, min, max);
+        scanf("%d", &num);
+        valida = validaEntrada(num, min, max);
+        if(valida == 0){
+            printf("[ERRO] Digite um numero valido!!!\n");
+        }
+    }while(valida == 0);
+
+    return num;
+}
+
+int validaJogada(int numJogador, int num, int *min, int *max){
+
+    if(numJogador < num){
+        *min = numJogador;
+    }
+    else{
+        *max = numJogador;
+    }
+}
+
+void status(int ok, int chance){
+
+    if(ok == 1){
+        printf("PARABENS Voce venceu com %d tentativas!!!\n\n", chance);
+    }
+    else{
+        printf("VOCE PERDEU mais sorte da proxima vez!!!\n\n");
+    }
+}
+
+int fimJogo(int chance, int num, int numJogador){
+    if(chance < 11){
+        if(num == numJogador){
+            return 1;
+        }
+    }
+    else if(chance > 10){
+        return 1;
+    }
+    return 0;
+}
+
+int jogada(){
+
+    int min = 1, max = 99;
+    int chance = 1;
+    int numJogador;
+    int numero = criarNumero();
+    int ok = 0;
+
+    do{
+        numJogador = entradaJogador(chance, min, max);
+        if(numJogador != numero){
+            validaJogada(numJogador, numero, &min, &max);
+            chance++;
+        }
+        else{
+            ok = 1;
+        }
+    }while(fimJogo(chance, numero, numJogador) == 0);
+
+    status(ok, chance);
+}
 
 int main(){
 
-    int numero = 0;
-    int gerado;
-    int max = 99, min = 1;
-    int cont = 1;
+    jogada();
 
-    srand(time(NULL));
-
-    printf("\n----Advinhe o numero gerado pelo computador----\n\n");
-
-    //valor aleatório gerado pelo computador
-    gerado = (rand() % 98) + 1;
-
-    //printf("\n%d\n", gerado);
-    while (numero != gerado)
-    {
-
-        printf("Tentativa [%d]: o numero esta entre %d e %d \n", cont, min, max);
-        scanf("%d", &numero);
-
-        //Verifica se está dentro da faixa de valores
-        while((numero < 1) || (numero > 99)){
-            printf("Digite um numero VALIDO!\n");
-            printf("Tentativa [%d]: o numero esta entre %d e %d \n", cont, min, max);
-            scanf("%d", &numero);
-        }
-
-        if((numero > min) && (numero < gerado)){
-            min = numero;
-        }
-        else if((numero < max) && (numero > gerado)){
-            max = numero;
-        }
-
-        if(numero != gerado){
-            cont++;
-        }
-
-        //Validação de parada
-        if(cont > 10){
-            gerado = numero;
-        }
-    }
-
-    if(cont < 11){
-        printf("\nResposta certa com [%d] tentativas :)\n\n", cont);
-    }
-    else{
-        printf("\nVoce excedeu o numero de tentativas :(\n\n");
-    }
-    
-    return 0;
+    return;
 }
